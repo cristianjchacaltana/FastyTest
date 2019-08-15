a3fe85e0a29dddd1db2effc7cfad0a2f29452d2f
cristian jeancarlo chacaltana fajardo <cristianjchacaltana@gmail.com>
2019-08-15 12:38:44 -0500

    fix: Correccion de Login, actualizacion de termino

--- app/src/main/java/com/firebaseapp/fasty/fastytest/activities/LoginActivity.java
+++ app/src/main/java/com/firebaseapp/fasty/fastytest/activities/LoginActivity.java
@@ -1,17 +1,24 @@
 package com.firebaseapp.fasty.fastytest.activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebaseapp.fasty.fastytest.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
@@ -22,6 +29,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText mPasswordEditText;
     private Button mLogInButton;
     private LinearLayout mSignUpLinearLayout;
+    private FirebaseAuth mFirebaseAuth;
 
 
 
@@ -35,10 +43,13 @@ public class LoginActivity extends AppCompatActivity {
         mLogInButton = findViewById(R.id.button_log_in);
         mSignUpLinearLayout = findViewById(R.id.linearlayout_sign_up);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
 
         initSignUp();
         enableLogIn();
+        signInFastyTest();
     }
 
 
@@ -90,6 +101,39 @@ public class LoginActivity extends AppCompatActivity {
 
         mEmailEditText.addTextChangedListener(textWatcher);
         mPasswordEditText.addTextChangedListener(textWatcher);
+
+    }
+
+
+    private void signInFastyTest(){
+
+        mLogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d(TAG, "onClick: hola peru");
+
+
+                mFirebaseAuth.signInWithEmailAndPassword(mEmailEditText.getText().toString(),
+                                                        mPasswordEditText.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if ( task.isSuccessful() ){
+                                    startActivity(new Intent(LoginActivity.this,
+                                                            MainActivity.class));
+                                } else {
+                                    Toast.makeText(LoginActivity.this,
+                                            "No se ha podido iniciar sesion",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                        });
+
+            }
+        });
 
     }
 
